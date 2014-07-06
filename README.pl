a8a2413f718ba77365c402cb34128fd74c830954
angrysoft <sebastian.zwierzchowski@gmail.com>
2014-07-06 22:54:51 +0000

    README.pl edited online with Bitbucket

---
+++ README.pl
@@ -0,0 +1,8 @@
+# README #
+Moduły pythona do manipulacji napisami do filmów.
+
+## modules:
+### subconverter:
+converts between various subtitle formats.
+### napi:
+Gets subtitles from www.napiprojekt.pl adapted to film.
