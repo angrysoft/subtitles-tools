6e2c5e5403b1e4f2222004b3c38aa9f9004e9396
Sebastian Zwierzchowski <sebastian.zwierzchowski@gmail.com>
2015-07-03 23:59:04 +0200

    up napi

--- apps/napi-cli.py
+++ apps/napi-cli.py
@@ -5,33 +5,31 @@ import os
 from threading import Thread
 from subtitlestools import napi
 
-def get_subtitle (filePath):
+def get_subtitle(filePath):
     """ Function doc """
     n = napi.Napi(filePath)
     n.setDebug(True)
-    #n.test()
     n.getSubtitles()
     del n
 
 def get_form_dir(dirPath):
-    movie = ('asf', 'avi', 'flv', 'mkv' , 'mov', 'mp4', 'mpe', 'mpg', 'mpeg','rmvb', 'wmv', '3pg', '3gp')
-    dirPath = dirPath.rstrip('/')
+    movie = ('asf', 'avi', 'flv', 'mkv' , 'mov', 'mp4', 'mpe', 'mpg', 'mpeg', 'rmvb', 'wmv', '3pg', '3gp')
+    dirPath = os.path.normpath(dirPath)
     for f in os.listdir(dirPath):
-        if f[0] =='.': continue
-        if os.path.isdir("{0}/{1}".format(dirPath,f)): continue
-        #file_list.append("{0}/{1}".format(dirPath,f))
-        suffix = ''
+        if f[0] == '.':
+            continue
+        if os.path.isdir(os.path.join(dirPath, f)):
+            continue
         if f.find('.') > 0:
-            suffix = f.rsplit('.',1)[1].lower()
-            if (suffix in movie):
+            suffix = f.rsplit('.', 1)[1].lower()
+            if suffix in movie:
                 try:
-                    Thread(target=get_subtitle, args=("{0}/{1}".format(dirPath,f),)).start()
+                    Thread(target=get_subtitle, args=(os.path.join(dirPath, f),)).start()
                 except Exception as errtxt:
                     print(errtxt)
 
-
 if __name__ == '__main__':
-    if(len(sys.argv)==1) or not os.path.exists(sys.argv[1]):
+    if(len(sys.argv) == 1) or not os.path.exists(sys.argv[1]):
         sys.exit(1)
     for f in sys.argv[1:]:
         if os.path.isdir(f):
